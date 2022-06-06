106be7c8fb06f7b1cd880ac1811da7125e0590f3
tweekes914 <100008459+tweekes914@users.noreply.github.com>
2022-06-06 03:31:17 -0400

    Delete posts directory

--- posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import URLPattern, path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('delete/<int:post_id>/', views.delete, name='delete'),
-    path('edit/<int:post_id>/', views.edit, name='edit'),
-    path('like/<int:post_id>/', views.like, name='like'),
-]
